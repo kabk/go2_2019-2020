ea859a54540e279a4de181834b0ca14e94751f67
JBG <jbg@peerparty.org>
2019-09-14 08:49:07 +0200

    cleanup

--- go2bot/irc.js
+++ go2bot/irc.js
@@ -18,21 +18,12 @@ rl.on('line', str => {
 })
 
 client.addListener('message', (from, to, message) => {
-  //console.log(from + ' => ' + to + ': ' + message)
   console.log(message)
 })
 
 client.addListener('pm', (from, message) => {
-  //console.log(from + ' => ME: ' + message)
   console.log(message)
 })
-
-/*
-client.addListener(`message${channel}`, (from, message) => {
-  //console.log(from + ' => peerparty: ' + message)
-  console.log(message)
-})
-*/
 
 client.addListener('error', message => {
   console.error('error: ', message)
