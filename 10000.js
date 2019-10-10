a8610eb7d2b3c2bcbb8599b348538e10165bb19a
JBG <jbg@peerparty.org>
2019-10-10 10:06:08 +0200

    10000 files example

--- 10000.js
+++ 10000.js
@@ -1,12 +1,9 @@
-const { exec } = require('child_process');
+const { exec } = require('child_process')
 
 async function run() {
   for(let i = 0; i < 10000; i++) {
     await exec(`touch output/${i}`, (err, stdout, stderr) => {
       if (err) return
-      // the *entire* stdout and stderr (buffered)
-      //console.log(`stdout: ${stdout}`)
-      //console.log(`stderr: ${stderr}`)
     })
   }
 }
